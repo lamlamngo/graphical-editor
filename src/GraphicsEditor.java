088f68e4b76d3f8c6b5e46d60fbf1a41bc1e5907
Lam Ngo <lamngo@Lams-MacBook-Pro.local>
2018-03-01 20:20:35 -0500

    add a private class ColorIndicator to show what the current color is

--- src/GraphicsEditor.java
+++ src/GraphicsEditor.java
@@ -1,7 +1,17 @@
 import java.awt.*;
 import javax.swing.*;
 
+import Editor.ColorIndicator;
+
 public class GraphicsEditor extends javax.swing.JFrame {
+	
+	private final int APPLET_WIDTH = 700, APPLET_HEIGHT = 500;
+	private final Color initialColor = Color.red; // default color starts as red
+	
+	private Command cmd; // the command being executed
+	private Drawing dwg; // the drawing: shapes in order
+	private ColorIndicator colorBox; // a GUI component to show the current default color
+	
     public GraphicsEditor() {
     		JFrame frame = new JFrame("Object-Oriented Graphical Editor");
         
@@ -23,6 +33,31 @@ public class GraphicsEditor extends javax.swing.JFrame {
     		
     		frame.setSize(300, 200);
     		frame.setVisible(true);
+    }
+    
+    /**
+     * A ColorIndicator shows what the current color is.
+     */
+    private class ColorIndicator extends JPanel {
+      private static final long serialVersionUID = 0;
+      
+      private final int COLORBOX_WIDTH = 20, COLORBOX_HEIGHT = 20;
+
+      /**
+       * Constructor sets the size and border.
+       */
+      public ColorIndicator() {
+        setBorder(BorderFactory.createEtchedBorder());
+        setPreferredSize(new Dimension(COLORBOX_WIDTH, COLORBOX_HEIGHT));
+      }
+
+      /**
+       * Show a new color.
+       * @param color the color to show
+       */
+      public void show(Color color) {
+        setBackground(color);
+      }
     }
     
 	public static void main(String[] args) {
